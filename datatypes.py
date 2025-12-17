7f4501b169ea2a0b709c66254cda9c4a99511126
Rawal Singh <rawalsinghr798@gmail.com>
2025-12-17 10:06:59 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+# Day 3: Data Types in Python
+
+# Integer
+a = 10
+print("a =", a)
+print("Type of a:", type(a))
+
+# Float
+b = 3.14
+print("b =", b)
+print("Type of b:", type(b))
+
+# String
+name = "Python"
+print("name =", name)
+print("Type of name:", type(name))
+
+# Boolean
+is_learning = True
+print("is_learning =", is_learning)
+print("Type of is_learning:", type(is_learning))
+
+# Taking input (always string)
+age = input("Enter your age: ")
+print("Age:", age)
+print("Type of age:", type(age))
+
+# Type conversion
+age_int = int(age)
+print("Age after conversion:", age_int)
+print("Type of age_int:", type(age_int))
